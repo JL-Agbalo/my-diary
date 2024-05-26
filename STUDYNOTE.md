9c8b3b4420f570b743479dcb14e9f54ffc132faa
fishagbalo <fishagbalo@gmail.com>
2024-05-26 23:43:57 +0800

    Testing CSS Types

--- STUDYNOTE.md
+++ STUDYNOTE.md
@@ -1,1 +1,21 @@
 Fragments = <>  </>
+
+
+Inline
+<!-- <div style={{color: 'red'}}>Footer</div> -->
+
+
+Javascript Object
+  <!-- const myFooter = {
+    color: 'white',
+    backgroundColor: 'black',
+    padding: '10px',
+  };
+  return (
+    // <div style={{color: 'red'}}>Footer</div>
+    <div style = {myFooter}>Footer</div>
+  )
+} -->
+
+
+CSS Stylesheet
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import './footer.css'
 const Footer = () => {
+  // const myFooter = {
+  //   color: 'white',
+  //   backgroundColor: 'black',
+  //   padding: '10px',
+  // };
   return (
-    <div>Footer</div>
+    // <div style={{color: 'red'}}>Footer</div>
+    <footer> 
+      <div className="myFooter">This is footer</div>
+    </footer>
   )
 }
 
