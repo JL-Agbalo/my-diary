a90e1400819dde2143b1781624b9f755231248d9
fishagbalo <fishagbalo@gmail.com>
2024-05-26 22:19:45 +0800

    Dynamic Data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 function App() {
+
+  const firstName = 'Lenard';
+  const lastName = 'Agbalo';
+  const age = 23;
   return (
   <div>
   <Header/>
   <main>
-    <h1>Hello World</h1>
+    <h1>Hello {firstName} {lastName} {age}</h1>
   </main>
   <Footer/>
 
